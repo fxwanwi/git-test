a42ccf71801b8a8327f48b8071269952e2c3b78f
VikFong <fxwanwi@163.com>
2022-09-28 19:39:24 +0800

    filst comat

---
+++ src/main/java/git/Gittest.java
@@ -0,0 +1,11 @@
+package git;
+
+/**
+ * @author shkstart
+ * @create 2022-09-28 18:39
+ */
+public class Gittest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+    }
+}
