0e20dee6df227e5f0ceed8de68a62732c94b4caf
VikFong <fxwanwi@163.com>
2022-09-28 19:50:58 +0800

    two comat

--- src/main/java/git/Gittest.java
+++ src/main/java/git/Gittest.java
@@ -9,5 +9,6 @@ public class Gittest {
         System.out.println("hello git!!!");
         System.out.println("hello git!!!");
         System.out.println("hello git!!!");
+        System.out.println("哈哈哈哈哈哈");
     }
 }
